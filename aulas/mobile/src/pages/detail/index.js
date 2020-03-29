4e539e3405afabcab3f3414ddd41d6c3cc0bb476
rafaelclaumann <rafael_claumann@hotmail.com>
2020-03-29 12:13:15 -0300

    conexao com o backend, transferindo informaçoes entre telas

--- aulas/mobile/src/pages/detail/index.js
+++ aulas/mobile/src/pages/detail/index.js
@@ -3,45 +3,50 @@ import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
 import { Feather } from '@expo/vector-icons';  // Father icons
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as MailComposer from 'expo-mail-composer';
 
 export default function Detail() {
     const navigation = useNavigation();
-    const message = "olá APAD, estou entrando em contato pois gostaria de ajudar no casop cadelinha tatropelada";
-
-    function navigateBack() {
-        navigation.goBack();
-    }
+    const { name, title, email, whatsapp, value, city, uf } = useRoute().params.incident;
+    const message = `OLá ${name} estou entrando em contato pois gostaria de
+        ajudar no caso ${title} com o valor de ${Intl
+            .NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+            .format(value)}`;
 
     function sendMail() {
         MailComposer.composeAsync({
-            subject: 'Heroi do caso: Cadelinha atropelada',
-            recipients: ['rafael_claumann@hotmail.com'],
-            body: message,
+            subject: `Heroi do caso: ${title}`,
+            recipients: [email],
+            body: message
         });
     }
 
     function sendWhatsapp() {
-        Linking.openURL(`whatsapp://send?phone=$+55048900000000&text=${message}`);
+        Linking.openURL(`whatsapp://send?phone=$+55${whatsapp}&text=${message}`);
     }
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
-                <TouchableOpacity onPress={navigateBack}>
+                <TouchableOpacity onPress={navigation.goBack}>
                     <Feather name="arrow-left" size={28} color="#e02041"></Feather>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.incident}>
                 <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={styles.incidentValue}>{name} de {city} {uf}</Text>
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Cadelinha atropelada.</Text>
+                <Text style={styles.incidentValue}>{title}</Text>
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>{Intl
+                    .NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
+                    .format(value)}</Text>
             </View>
 
             <View style={styles.contactBox}>
