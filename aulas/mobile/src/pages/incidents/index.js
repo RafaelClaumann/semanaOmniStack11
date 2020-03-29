26d41d7b1e95558a76790a432f719da9105d11ae
rafaelclaumann <rafael_claumann@hotmail.com>
2020-03-29 13:19:03 -0300

    scroll infinito nos incidents

--- aulas/mobile/src/pages/incidents/index.js
+++ aulas/mobile/src/pages/incidents/index.js
@@ -10,13 +10,28 @@ export default function Incidents() {
     const navigation = useNavigation();
     const [incidents, setIncidents] = useState([]);
     const [totalIncidents, setTotalIncidents] = useState(0);
-    
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    async function loadIncidents() {
+        if (loading) { return; }
+
+        if (totalIncidents > 0 && incidents.length === totalIncidents) {
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('incidents', { params: { page } });
+        setIncidents([...incidents, ...response.data]);
+        setTotalIncidents(response.headers['x-total-count']);
+
+        setPage(page + 1);
+        setLoading(false);
+    }
+
     useEffect(() => {
-        api.get('incidents')
-            .then(response => {
-                setIncidents(response.data);
-                setTotalIncidents(response.headers['x-total-count']);
-            });
+        loadIncidents();
     }, []);
 
     return (
@@ -35,6 +50,8 @@ export default function Incidents() {
                 style={styles.incidentsList}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
