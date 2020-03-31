7ddd9d9c72e976372e83fe80859c5f3a2750ad29
rafaelclaumann <rafael_claumann@hotmail.com>
2020-03-31 15:43:56 -0300

    testes de integracao

---
+++ aulas/backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();  // reset tables on DB
+        await connection.migrate.latest();  // create tables on DB
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('Should be able to create a new ONG.', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            //.set('Authorization', 'ongId')
+            .send({
+                name: 'APAD 10',
+                email: 'apad10@email.com',
+                whatsapp: '48000000000',
+                city: 'Florianópolis',
+                uf: 'sc'
+            });
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    })
+});
